45768e460bf56d87a3152b1e47d243bf7184139f
Никита Киселев <nikitakiselev@MacBook-Air-Nikita.local>
2025-03-18 18:20:15 +0300

    Initial commit

---
+++ MyWardrobe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWardrobe
+//
+//  Created by Никита Киселев on 18.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyWardrobeTests/MyWardrobeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyWardrobeTests.swift
+//  MyWardrobeTests
+//
+//  Created by Никита Киселев on 18.03.2025.
+//
+
+import Testing
+@testable import MyWardrobe
+
+struct MyWardrobeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
